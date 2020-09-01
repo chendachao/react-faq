e7bb72d24aad519548e0b3686aadb4288701785f
Dachao Chen <dachao.chen@coupang.com>
2020-09-01 10:46:23 +0800

    style chores

--- example/README.md
+++ example/README.md
@@ -3,3 +3,8 @@ This example was bootstrapped with [Create React App](https://github.com/faceboo
 It is linked to the react-faq package in the parent directory for development purposes.
 
 You can run `yarn install` and then `yarn start` to test your package.
+
+
+Resolve yarn install no such file or directory issue
+- https://github.com/yarnpkg/yarn/issues/4563#issuecomment-552256526
+
--- example/src/app/hooks/useFetch.js
+++ example/src/app/hooks/useFetch.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 // https://www.pluralsight.com/guides/hierarchy-of-components-and-how-to-get-async-data
-export function useFetch({url}) {
+export function useFetch({url, options}) {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
       setLoading(true);
-      const response = await axios.get(url);
+      const response = await axios.get(url, {
+        ...options
+      });
       setLoading(false);
       setData(response.data);
     })();
